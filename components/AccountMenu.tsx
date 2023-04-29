86c47fcfdd310c3a27f11d16a524e6d8696d86fa
Your Name <chetandasa2814@gmail.com>
2023-04-29 19:43:27 +0530

    TV Shows

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -24,7 +24,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
       </div>
       <hr className="bg-gray-600 border-0 h-px my-4" />
       <div onClick={() => signOut()} className="px-3 text-center text-white text-sm hover:underline">
-        Sign out of Netflix
+        Sign out of TV Shows
       </div>
     </div>
   )
